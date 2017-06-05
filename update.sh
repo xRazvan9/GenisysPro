0f336b4ab95d3913ecf3104233f3f4903042459a
xRazvan9 <xrazvan9@gmail.com>
2017-06-05 15:07:37 +0300

    Auto update

---
+++ update.sh
@@ -0,0 +1,1 @@
+echo "Updating...." && echo "<#######################>" && rm -rf GenisysPro && git clone https://github.com/GenisysPro/GenisysPro.git && rm -rf src && mkdir src && cd GenisysPro/src && mv * ../src
